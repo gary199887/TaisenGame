c1994ea5993a1aa5e62df1268b60cd4f880fcf3c
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-07-11 19:42:32 +0900

    ダイアログシステム関連機能追加＆バグ改修 ー文を一文字ずつ増えて表示する機能追加 ー操作関連バグ
    捜査パート
    -rigidBodyのsleep modeをnever sleepに変更
    
    スクリプト中にコメントちょくちょく追加

--- Assets/Scripts/GameScene/ItemDetailUIManager.cs
+++ Assets/Scripts/GameScene/ItemDetailUIManager.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class ItemDetailUIManager : MonoBehaviour
 {
+    // game objects / components
     public GameObject itemDetail;
     public Text itemName;
     public Text itemDescription;
-    public static ItemList itemList;
-    float cd;
-    float timeCount;
+    
+    public static ItemList itemList;        // item list for current stage, be set by GameDirector(loading stage data from file)
+    float cd;                               // gap(time->sec) between two actions
+    float timeCount;                        // timer
     // Start is called before the first frame update
     void Start()
     {
@@ -24,23 +26,31 @@ public class ItemDetailUIManager : MonoBehaviour
         {
             timeCount += Time.deltaTime;
             if(timeCount > cd)
-            if (Input.GetKeyDown(KeyCode.Z))
+            if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X))
             {
                 itemDetail.SetActive(false);
                 PlayerController.canMove = true;
             }
         }
     }
+
+    // start item detail UI
     public void showItemDetail(int id) {
-        PlayerController.canMove = false;
-        timeCount = 0;
-        Item item = itemList.items[id];
+        PlayerController.canMove = false;   // stop player's movement
+        timeCount = 0;                      // clear timer
+        Item item = itemList.items[id];     // get the item data by id
+      
+        // set the detail data to corresponded gameObj
         itemName.text = item.name;
         itemDescription.text = item.description;
+
+        // set the item to player's itemBox
         PlayerController.itemBox[id] = item;
-        //foreach (Item value in PlayerController.itemBox) {
+        //foreach (Item value in PlayerController.itemBox) {    // check if the item is in itemBox(printed as "null" if it is not)
         //    Debug.Log(value);
         //} ;
+
+        // make item detail UI visible
         itemDetail.SetActive(true);
     }
 
--- Assets/Scripts/GameScene/TalkSystemManager.cs
+++ Assets/Scripts/GameScene/TalkSystemManager.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TalkSystemManager : MonoBehaviour
 {
     public GameObject talkSystem;
-    DialogManager dialogManager;
-    public static List<Chara> charaList;
-    public static int currentChosen;
-    public Text[] charaDetailText;
-    float timeCount;
-    const float cd = 0.2f;
+    DialogManager dialogManager;            // dialog manager for openning control
+    public static List<Chara> charaList;    // charaList for the stage, be set in GameDirector(after loading data from file)
+    public static int currentChosen;        // index of the chara chosen currently
+    public Text[] charaDetailText;          // UI components, 0:name  1:gender  2:relationShip  3:secret
+    float timeCount;                        // timeCounter
+    const float cd = 0.2f;                  // gap between two actions(prevent making multiple actions by the same key-down)
     private void Start()
     {
+        // clear timer and initialize data when scene loaded
         currentChosen = 0;
         talkSystem.SetActive(false);
         timeCount = 0;
@@ -23,42 +25,51 @@ public class TalkSystemManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!talkSystem.activeSelf) return;
+        if (!talkSystem.activeSelf) return;     // do nothing when talk system is not active
+        
+        // can do some actions only if talk system is active
         timeCount += Time.deltaTime;
-        if (timeCount > cd)  //to make a gap between choice changed or other actions
+        if (timeCount > cd)  // to make a gap between choice changed or other actions
         {
+            if(!dialogManager.dialog.activeSelf)
             if (Input.GetAxis("Horizontal") > 0.1)  // get next chara
             {
                 startTalk(currentChosen == charaList.Count - 1 ? currentChosen = 0 : ++currentChosen);
-                timeCount = 0;
+                timeCount = 0;  // clear counter when whatever actions had been taken
             }
             else if (Input.GetAxis("Horizontal") < -0.1)
             {
                 startTalk(currentChosen == 0 ? currentChosen = charaList.Count - 1 : --currentChosen);
                 timeCount = 0;
             }
-            else if (Input.GetKeyDown(KeyCode.X))  //move back to detect part
+            else if (Input.GetKeyDown(KeyCode.X))  // move back to detect part
             {
                 PlayerController.canMove = true;
                 timeCount = 0;
                 talkSystem.SetActive(false);
             }
-            else if (Input.GetKeyDown(KeyCode.Z))  //talk with chosen charactor
+            else if (Input.GetKeyDown(KeyCode.Z))  // talk with chosen charactor
             {
                 timeCount = 0;
-                dialogManager.showDialog(charaList[currentChosen].talks);
+                dialogManager.showDialog(charaList[currentChosen].talks);   // throw the talk array(string[]) of current chosen charactor to dialog system
             }
         } 
     }
 
+    // start talk system
     public void startTalk(int choose) {
-        PlayerController.canMove = false;
+        PlayerController.canMove = false;   // make player connot do any action
         Chara chara = charaList[choose];
+
+        // set charactor detail to text UI
         charaDetailText[0].text = chara.name; 
         charaDetailText[1].text = $"性別：{chara.gender}";
         charaDetailText[2].text = $"人間関係：\n{chara.relationShip}";
         charaDetailText[3].text = $"秘密：\n{chara.secret}";
+        
+        // clear timer
         timeCount = 0;
+        // then start talk system
         talkSystem.SetActive(true);
     }
 }
