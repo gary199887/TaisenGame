eb8839665917d3d05c48a4d79a1ad35461a368f2
gary199887 <47510884+gary199887@users.noreply.github.com>
2023-07-18 15:44:05 +0900

    add comment into resultDirector.cs

--- Assets/Scripts/ResultScene/ResultDirector.cs
+++ Assets/Scripts/ResultScene/ResultDirector.cs
@@ -7,34 +7,51 @@ using UnityEngine.UI;
 
 public class ResultDirector : CommonFunctions
 {
-    public Text stageNameUI;
+    // current data UI part
+    public Text stageNameUI;        // reveal the stage name such as "Stage1", "Stage2"
     public Text timeUI;
     public Text nameUI;
-    public Text[] rankNameUI;
-    public Text[] rankTimeUI;
     public InputField nameInput;
+   
+    public Text[] rankNameUI;       // name of rank 1~5
+    public Text[] rankTimeUI;       // play time of rank 1~5
+
+    // hint the key to press
     public GameObject anyKey;
+    // to move the whole two parts
     public GameObject currentData, rankList;
+    // two different hints to identify if the current record is in rank
     public GameObject newRecord, outOfRank;
+
+    // local datas
     Rank rank;
     Record currentRecord;
-    bool canJump;
-    bool startMove;
-    bool isNewRecord;
+    bool canJump;       // whether can jump to Title Scene or not
+    bool startMove;     // whether cuurent data and rank list start to move left
+    bool isNewRecord;   // whether curren data is a new record
     // Start is called before the first frame update
     void Start()
     {
+        // initialize local booleans and several gameobjects as false
         canJump = false;
         startMove = false;
         isNewRecord = false;
-        stageNameUI.text = $"Stage{GameDirector.stage}";
-        timeUI.text = "TIME:  " + GameDirector.getTimeString();
-        nameInput.Select();
-        rank = GameDirector.getRank();
         anyKey.SetActive(false);
         newRecord.SetActive(false);
         outOfRank.SetActive(false);
+
+        // display data from game director
+        stageNameUI.text = $"Stage{GameDirector.stage}";
+        timeUI.text = "TIME:  " + GameDirector.getTimeString();
+        rank = GameDirector.getRank();
+
+        // select input field for entering player's name
+        nameInput.Select();
+        
         if (rank == null) rank = new Rank();    // if there is no file at the path, create a new empty rank
+        
+        // if current play time is longer than the last one in rank,
+        // skip name-input and just wait for rank sliding out
         if (rank.records.Count >= 5)
         {
             Record lastRecord = rank.records[rank.records.Count - 1];
@@ -48,6 +65,7 @@ public class ResultDirector : CommonFunctions
     // Update is called once per frame
     void Update()
     {
+        // not allowed to jump to title immediately
         if (canJump)
         {
             if (Input.anyKeyDown) {
@@ -56,6 +74,8 @@ public class ResultDirector : CommonFunctions
             tenmetsu(anyKey);
 
         }
+
+        // move current data part and rank list slide left
         if (startMove) {
 
             float speed = 10f;
@@ -65,37 +85,48 @@ public class ResultDirector : CommonFunctions
                 rankList.transform.Translate(-speed, 0, 0);
             } else startMove = false;
         }
+
+        // turn on/off "new record!!" hint if this time is a new record
         if (isNewRecord) tenmetsu(newRecord);
 
         endGame();
     }
 
     public void onEndEnteringName() {
-        string name = nameInput.text.Substring(0, Mathf.Clamp(nameInput.text.Length, 0, 10));     //name length limitation in 8
+        // name length limitation in 10
+        string name = nameInput.text.Substring(0, Mathf.Clamp(nameInput.text.Length, 0, 10));
         if (!nameInput.text.Equals("")) GameDirector.setPlayerName(name);
         
+        // update current data into entered name / default name from game director
         nameUI.text = "NAME:  " + GameDirector.playerName;
         nameInput.gameObject.SetActive(false);
+
         currentRecord = new Record(GameDirector.playerName, GameDirector.gameTime);      // create new record instance with current data
         
-        
+        // add current record and return a boolean if this time is in range of top 5
         if (rank.addAndSortRecord(currentRecord)) {
+            // if it is, move the new record hint to the corresponding position
             newRecord.transform.position = rankNameUI[rank.records.IndexOf(currentRecord)].gameObject.transform.position - new Vector3(350, 0, 0);
             isNewRecord = true;
 
         } else outOfRank.SetActive(true);
         GameDirector.saveRank(rank);
         
-        displayRank();                  // print rank data in the rank list on result
+        displayRank();                  // print rank data into corresponding UI
+
         StartCoroutine(DelayMethod(2.5f, () =>
-        {    // make a gap to wait for rank being displayed
+        {    // start sliding after waiting for 2.5 sec
             startMove = true;
         }));
-        StartCoroutine(DelayMethod(4.5f, () => {    // make a gap to wait for rank being displayed
+
+        
+        StartCoroutine(DelayMethod(4.5f, () => {    
+            // make title scene possible to be loaded after 4.5 sec
             canJump = true;
         }));
     }
 
+    // print rank data into corresponding UI
     void displayRank() {
         for (int i = 0; i < rank.records.Count; i++) {
             rankNameUI[i].text = rank.records[i].name;
