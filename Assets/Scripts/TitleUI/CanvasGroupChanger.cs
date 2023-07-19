a7041b070dd699b60428fb73c7c72ad96a725827
Ci0217 <22ci0217@jec.ac.jp>
2023-07-19 16:53:41 +0900

    タイトルの配置と機能設定 ステージ変更時にエラーが出る

---
+++ Assets/Scripts/TitleUI/CanvasGroupChanger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Reflection;
+using Unity.VisualScripting;
+
+public class CanvasGroupChanger : MonoBehaviour, Button
+{
+    [SerializeField]
+    CanvasGroup now;
+
+    [SerializeField]
+    CanvasGroup next;
+
+    [Range(0,10)]
+    [SerializeField]
+    float direction;
+
+    [SerializeField]
+    Ease nowCvGp;
+    [SerializeField]
+    Ease nextCvGp;
+    
+    public void onClicked()
+    {
+        TitleCursor.canMove = false;
+        next.gameObject.SetActive(true);
+        now.DOFade(0, direction).SetEase(nowCvGp)
+       .OnComplete
+       (
+            () =>
+            {
+                next.DOFade(1, direction).SetEase(nextCvGp).OnComplete
+                (
+                    () =>
+                    {
+
+                        now.gameObject.SetActive(false);
+                        TitleCursor.canMove = true;
+                    }
+               );
+            }
+       );
+    }
+}
