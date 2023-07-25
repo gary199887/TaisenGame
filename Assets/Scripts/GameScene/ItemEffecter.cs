c9c93eb1939689f9a989fa134780c56b580e7936
CI <CI@jec.ac.jp>
2023-07-25 16:42:41 +0900

    アイテム待機画面に明滅する光画像を入れた

--- Assets/Scripts/GameScene/ItemEffecter.cs
+++ Assets/Scripts/GameScene/ItemEffecter.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemEffecter : MonoBehaviour
 {
+    SpriteRenderer sprd;
+
+    [SerializeField]
+    Ease ease;
+    [SerializeField]
+    float direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprd = GetComponent<SpriteRenderer>();
+        sprd.DOFade(0, direction).SetLoops(-1, LoopType.Yoyo);    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
